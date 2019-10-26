44f26a84035466133dd55e33c6542b64e511ddc2
Pary Ly <paleepary@aol.com>
2019-10-26 13:26:23 -0700

    fixed undefined error

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-var firebaseConfig = {
+var Config = {
   apiKey: "AIzaSyDQ66iMF_wsL_F29Bk23DvD6R0nf63bFkY",
   authDomain: "group2-employment-tracker.firebaseapp.com",
   databaseURL: "https://group2-employment-tracker.firebaseio.com",
@@ -8,30 +8,32 @@ var firebaseConfig = {
   appId: "1:401541146441:web:6d65761407c554d1de9c98"
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(Config);
 
 var database = firebase.database();
 
-  database.ref().push({
-    employeeName: "",
-    role: "",
-    startDate: "",
-    monthlyRate: 0,
-  })
+
 
   database.ref().on("value", function(snapshot) {
     
-
-
   });
 
-  var name = $("#employee-name").val()
-  var role = $("#employee-role").val()
-  var startDate = $("#start-date").val()
-  var rate = $("#monthly-rate").val()
+
   
 
-  $("#run-search").on("click", function(){
-    console.log(name);
-    console.log(role);
+  $("#submit-button").on("click", function(event){
+    event.preventDefault();
+    var name = $("#employee-name").val()
+    var role = $("#employee-role").val()
+    var startDate = $("#start-date").val()
+    var rate = $("#monthly-rate").val()
+    database.ref().push({
+        employeeName: "",
+        role: "",
+        startDate: "",
+        monthlyRate: 0,
+      })
+      console.log(name);
+        console.log(role);
   });
