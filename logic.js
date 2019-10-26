907d965ca69833734400f72a2b714c5ec5d0dae0
James Bajamundi <jamesbjmnd@gmail.com>
2019-10-26 11:30:30 -0700

    changes

--- logic.js
+++ logic.js
@@ -1,13 +1,13 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDQ66iMF_wsL_F29Bk23DvD6R0nf63bFkY",
-    authDomain: "group2-employment-tracker.firebaseapp.com",
-    databaseURL: "https://group2-employment-tracker.firebaseio.com",
-    projectId: "group2-employment-tracker",
-    storageBucket: "group2-employment-tracker.appspot.com",
-    messagingSenderId: "401541146441",
-    appId: "1:401541146441:web:6d65761407c554d1de9c98"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDQ66iMF_wsL_F29Bk23DvD6R0nf63bFkY",
+  authDomain: "group2-employment-tracker.firebaseapp.com",
+  databaseURL: "https://group2-employment-tracker.firebaseio.com",
+  projectId: "group2-employment-tracker",
+  storageBucket: "group2-employment-tracker.appspot.com",
+  messagingSenderId: "401541146441",
+  appId: "1:401541146441:web:6d65761407c554d1de9c98"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+var database = firebase.database();
